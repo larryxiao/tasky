9e969459829da52904ee27ca8ab90409fb205a76
Xiao Di <xiaodi@sjtu.edu.cn>
2013-11-08 21:01:11 +0800

    fix #3
    use google oauth
    
    tasky_dir = os.environ['HOME'] + '/.tasky'
    put your "client_secrets.json" file in tasky_dir
    the process save your credentials also in tasky_dir
    
    export service

--- src/tasky.py
+++ src/tasky.py
@@ -18,6 +18,10 @@ from collections import OrderedDict
 from oauth2client.client import OAuth2WebServerFlow
 from oauth2client.file import Storage
 from oauth2client.tools import run
+from apiclient import discovery
+from oauth2client import file
+from oauth2client import client
+from oauth2client import tools
 
 import datetime as dt
 import httplib2
@@ -25,6 +29,7 @@ import os
 import shlex
 import sys
 import time
+import argparse
 # import json # TODO
 
 tasky_dir = os.environ['HOME'] + '/.tasky'
@@ -435,32 +440,50 @@ class Auth():
     def get_API_key(self):
         return self.apikey
 
+# Parser for command-line arguments.
+parser = argparse.ArgumentParser(
+    description=__doc__,
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    parents=[tools.argparser])
+
+
+# CLIENT_SECRETS is name of a file containing the OAuth 2.0 information for this
+# application, including client_id and client_secret. You can see the Client ID
+# and Client secret on the APIs page in the Cloud Console:
+# <https://cloud.google.com/console#/project/995681773719/apiui>
+CLIENT_SECRETS = os.path.join(tasky_dir, 'client_secrets.json')
+
+# Set up a Flow object to be used for authentication.
+# Add one or more of the following scopes. PLEASE ONLY ADD THE SCOPES YOU
+# NEED. For more information on using scopes please see
+# <https://developers.google.com/+/best-practices>.
+FLOW = client.flow_from_clientsecrets(CLIENT_SECRETS,
+  scope=[
+      'https://www.googleapis.com/auth/tasks',
+      'https://www.googleapis.com/auth/tasks.readonly',
+    ],
+    message=tools.message_if_missing(CLIENT_SECRETS))
+
 def authenticate():
-    global service
-    f = Auth(tasky_dir + '/keys.txt')
+  global service
+  # Parse the command-line flags.
+  flags = parser.parse_args("")
 
-    # OAuth 2.0 Authentication
-    FLOW = OAuth2WebServerFlow(
-        client_id=f.get_client_ID(),
-        client_secret=f.get_client_secret(),
-        scope='https://www.googleapis.com/auth/tasks',
-        user_agent='Tasky/v1')
+  # If the credentials don't exist or are invalid run through the native client
+  # flow. The Storage object will ensure that if successful the good
+  # credentials will get written back to the file.
+  storage = file.Storage(os.path.join(tasky_dir, 'tasks.dat'))
+  credentials = storage.get()
+  if credentials is None or credentials.invalid:
+    credentials = tools.run_flow(FLOW, storage, flags)
 
-    # If credentials don't exist or are invalid, run through the native client
-    # flow. The Storage object will ensure that if successful the good
-    # Credentials will get written back to a file.
-    storage = Storage(tasky_dir + '/tasks.dat')
-    credentials = storage.get()
+  # Create an httplib2.Http object to handle our HTTP requests and authorize it
+  # with our good Credentials.
+  http = httplib2.Http()
+  http = credentials.authorize(http)
 
-    if credentials is None or credentials.invalid:
-        credentials = run(FLOW, storage)
-
-    http = httplib2.Http()
-    http = credentials.authorize(http)
-
-    # The main Tasks API object
-    service = build(serviceName='tasks', version='v1', http=http,
-        developerKey=f.get_API_key())
+  # Construct the service object for the interacting with the Tasks API.
+  service = discovery.build('tasks', 'v1', http=http)
 
 def main(args):
     get_data()
